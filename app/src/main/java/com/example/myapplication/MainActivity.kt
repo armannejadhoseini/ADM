0f4cde99b3bbb210610c47fca207f31686daf269
armannejadhoseini <68272842+armannejadhoseini@users.noreply.github.com>
2022-01-10 12:58:50 +0330

    added fab

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var fabVisibility = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +24,19 @@ class MainActivity : AppCompatActivity() {
 
         val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar, 0, 0)
         toggle.syncState()
+
+        val floatingActionButton = binding.floatingActionButton
+        floatingActionButton.setOnClickListener {
+            if (fabVisibility == true) {
+                binding.fab1.isVisible = false
+                binding.fab2.isVisible = false
+                fabVisibility = false
+            } else {
+                binding.fab1.isVisible = true
+                binding.fab2.isVisible = true
+                fabVisibility = true
+            }
+        }
 
 
 
