b9ecb2a6727d0d3a20414938c72e9fe7f7fd8223
armannejadhoseini <68272842+armannejadhoseini@users.noreply.github.com>
2022-01-10 10:49:44 +0330

    update

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,18 +1,9 @@
 package com.example.myapplication
 
-import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuInflater
-import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
+
 import com.example.myapplication.databinding.ActivityMainBinding
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.SimpleExoPlayer
-import java.net.URI
-import android.R
 
 class MainActivity : AppCompatActivity() {
 
