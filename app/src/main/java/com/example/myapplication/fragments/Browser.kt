7a7f879cfdce85665e0a8f36557dc58c0fb1331d
armannejadhoseini <68272842+armannejadhoseini@users.noreply.github.com>
2022-01-11 11:51:45 +0330

    link ui

--- app/src/main/java/com/example/myapplication/fragments/Browser.kt
+++ app/src/main/java/com/example/myapplication/fragments/Browser.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.example.myapplication.databinding.BrowserBinding
 
@@ -24,6 +25,14 @@ class Browser : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val webView = binding.webView
+
+        //webView settings
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+
+        //load google by default
+        webView.loadUrl("https://www.google.com/")
     }
 
     override fun onDestroyView() {
