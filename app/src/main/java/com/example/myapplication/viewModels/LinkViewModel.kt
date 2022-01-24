e1901653b063392c73bdd9410fdcf6f75aa846ba
armannejadhoseini <68272842+armannejadhoseini@users.noreply.github.com>
2022-01-24 14:35:04 +0330

    updated finished section

--- app/src/main/java/com/example/myapplication/viewModels/LinkViewModel.kt
+++ app/src/main/java/com/example/myapplication/viewModels/LinkViewModel.kt
@@ -6,11 +6,14 @@ import android.webkit.MimeTypeMap
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.domain.model.DownloadLog
 import com.example.domain.model.downloadFile
 import com.example.domain.usecase.addToQueueImpl
 import com.example.domain.usecase.downloadImpl
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.io.File
 import java.util.*
 import javax.inject.Inject
@@ -20,7 +23,7 @@ import javax.inject.Inject
 class LinkViewModel @Inject constructor(
     val downloadImpl: downloadImpl,
     val addToQueueImpl: addToQueueImpl
-): ViewModel() {
+) : ViewModel() {
     private var _url = MutableLiveData<String>()
     val url get() = _url
 
@@ -39,16 +42,18 @@ class LinkViewModel @Inject constructor(
         val downloadFile = downloadFile(fileName.value!!, url.value!!, time, mimeType.value!!)
 
         //call the usecase to add the download to queue
-        addToQueueImpl.add(
-            //make a log
-            DownloadLog(
-                0,
-                downloadFile.fileName,
-                downloadFile.url,
-                downloadFile.time,
-                downloadFile.mimeType
+        viewModelScope.launch(Dispatchers.IO) {
+            addToQueueImpl.add(
+                //make a log
+                DownloadLog(
+                    0,
+                    downloadFile.fileName,
+                    downloadFile.url,
+                    downloadFile.time,
+                    downloadFile.mimeType
+                )
             )
-        )
+        }
 
         //call the download usecase
         downloadImpl.download(downloadFile)
